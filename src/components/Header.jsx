970cb7005145b2a82fcbbdc85992a0d3b5ac9777
Paulo <paulorfsantos17@gmail.com>
2023-11-22 19:46:40 -0300

    feat:  add style and logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import   style from'./Header.module.css'
+import iginiteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
   return(
     <header className={style.header}>
+      <img src={iginiteLogo} alt="Logotipo do Ignite" />
       <strong >Ingite Feed</strong>
     </header>
   )
